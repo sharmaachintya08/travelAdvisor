28366603337315b80fa7e9a482507ee1f517bb47
sharmaachintya08 <sharmachintya08@gmail.com>
2022-03-10 13:55:18 +0530

    added the location coordinates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import Navbar from './components/header/Navbar';
 import Grid from "@mui/material/Grid";
 import Map from './components/map/Map';
@@ -6,17 +6,31 @@ import List from './components/list/List';
 import Restaurants from './components/api/Restaurants';
 
 const App = () => {
+
+
+    const [bounds,setBounds] = useState(null);
+    const [coordinates,setCoordinates] = useState({});
+
+
+    console.log(bounds);
+
+    useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+      });
+    },[]);
     useEffect(()=>{
         Restaurants().then((data)=>{
             console.log(data);
         })
     },[]);
+
     return(
       <React.Fragment>
         <Navbar />
         <Grid container>
           <Grid item xs={12} md={8} sm={6}>
-            <Map />
+            <Map coordinates={coordinates} setCoordinates={setCoordinates} setBounds={setBounds}/>
           </Grid>
           <Grid item xs={12} md={4} sm={6}>
             <List />
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const Map = () => {
+const Map = ({coordinates,setCoordinates,setBounds}) => {
+    console.log('map: ',coordinates);
+
     return (
       <div style={{ height: "90vh", width: "100%" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyAsN1F5CXRuS74oK1BY68_gGlWjkRPIvy0" }}
-          defaultCenter={{ lat: 0, lng: 0 }}
+          defaultCenter={coordinates}
+          center={coordinates}
           defaultZoom={8}
+          onChange={(e)=>{
+              setCoordinates({lat:e.center.lat,lng:e.center.lng})
+              setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+          }}
         >
           
         </GoogleMapReact>
